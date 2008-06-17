9cf2cc083979fafa970ed8eb6e19c7822e5cdf70
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-17 10:32:44 +0200

    Code for the plugin ...

--- lib/simplified_breadcrumbs.rb
+++ lib/simplified_breadcrumbs.rb
@@ -1,1 +1,32 @@
-# SimplifiedBreadcrumbs
+module SimplifiedBreadcrumbs
+
+  class ActionController::Base
+
+    protected
+
+    def add_breadcrumb name, url = ''
+      @breadcrumbs ||= []
+      url = eval(url) if url =~ /_path|_url/
+      @breadcrumbs << [name, url]
+    end
+
+    def self.add_breadcrumb name, url, options = {}
+      before_filter options do |controller|
+        controller.send(:add_breadcrumb, name, url)
+      end
+    end
+
+  end
+
+  module Helper
+
+    def breadcrumbs(separator = "&rsaquo;")
+      @breadcrumbs.map { |txt, path| link_to_unless(path.blank?, h(txt), path) }.join(" #{separator} ")
+    end
+
+  end
+
+end
+
+ActionController::Base.send(:include, SimplifiedBreadcrumbs)
+ActionView::Base.send(:include, SimplifiedBreadcrumbs::Helper)
